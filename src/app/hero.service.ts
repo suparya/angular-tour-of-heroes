0fda7cd67009dd6a0fa2b8391a4ed0eb7c0bbfc6
suparya <suparya.das@hirepro.in>
2018-10-22 09:54:29 +0530

    add: routing and  peer details and msg component to communicate.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,22 +1,79 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   // typical "service-in-service" scenario
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
-   // Service could get data from anywhere > a web service, local storage, or a mock data source.
-  //to make an asynchronous service call
+  /** Service could get data from anywhere > a web service, local storage, or a mock data source.
+
+  /*to make an asynchronous service call , fetching flat mock data 
+  //getHeroes(): Observable<Hero[]> {
+  // 	// TODO: send the message _after_ fetching the heroes
+  // this.messageService.add('HeroService: fetched heroes');
+  // return of(HEROES);
+  // }
+
+   getHero(id: number):Observable<Hero>{
+   this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** GET heroes from the server */
+   private heroesUrl = 'api/heroes';
+
   getHeroes(): Observable<Hero[]> {
-  	// TODO: send the message _after_ fetching the heroes
-  this.messageService.add('HeroService: fetched heroes');
-  return of(HEROES);
-}
+  return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+        tap(heroes => this.log('fetched heroes')),
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+
+ private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+ }
+
+   /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+     getHero(id: number):Observable<Hero>{
+   this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
 }
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,7 +13,7 @@ export class HeroesComponent implements OnInit {
   	id: 1,
     name: 'Windstorm'
   };
-  selectedHero: Hero;
+
   heroes:Hero[];
 
   constructor(private heroService: HeroService) {}
@@ -26,9 +26,4 @@ export class HeroesComponent implements OnInit {
   this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero):void{
-     this.selectedHero = hero;
-  }
-
 }
